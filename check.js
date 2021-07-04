31599341e2069b59e4340626db02430b74c48235
kafka <sajon.tahsen@gmail.com>
2021-07-04 17:07:51 +0600

    session cookie login implemented

--- check.js
+++ check.js
@@ -19,9 +19,9 @@ let creds = getCreds.split(" ");
 console.log(creds);
 
 if(values[1] === creds[1]){
-    localStorage.setItem("loggedIn", "true");
-    localStorage.setItem("username", creds[0]);
-    localStorage.setItem("usertype", creds[2]);
+    sessionStorage.setItem("loggedIn", "true");
+    sessionStorage.setItem("username", creds[0]);
+    sessionStorage.setItem("usertype", creds[2]);
     window.location.replace('./index.html');
 }else{
     window.location.replace('./wrong_creds.html')
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const stuff = document.getElementById('stuff');
-const loggedIn = localStorage.getItem('loggedIn');
+const loggedIn = sessionStorage.getItem('loggedIn');
 
 if(loggedIn === "true"){
-    const username = localStorage.getItem('username');
+    const username = sessionStorage.getItem('username');
     stuff.innerHTML += `<h1>Welcome, ${username}</h1>`;
     // stuff.append(document.createElement('br'));
-    stuff.innerHTML += `<h1>You are an ${localStorage.getItem('usertype')}!</h1>`
+    stuff.innerHTML += `<h1>You are an ${sessionStorage.getItem('usertype')}!</h1>`
     stuff.innerHTML += "<h1><a href='login.html' id='logout'>Logout</a></h1>";
 
     const logout = document.getElementById('logout');
     logout.addEventListener('click', function(){
-        localStorage.removeItem('loggedIn');
-        localStorage.removeItem('username');
-        localStorage.removeItem('usertype');
+        sessionStorage.removeItem('loggedIn');
+        sessionStorage.removeItem('username');
+        sessionStorage.removeItem('usertype');
 })
 }
 else{
