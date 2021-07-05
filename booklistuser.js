ce6b8ade75f3f3142fab16c7bec980e89307dfa0
kafka <sajon.tahsen@gmail.com>
2021-07-05 15:55:35 +0600

    Added basic bookstore functionality

---
+++ booklistuser.js
@@ -0,0 +1,39 @@
+document.getElementById('logout').addEventListener('click', logout);
+document.getElementById('user').innerText = `Welcome, ${localStorage.getItem('username')}!`;
+
+function logout(){
+    localStorage.removeItem('loggedIn');
+    localStorage.removeItem('username');
+    localStorage.removeItem('usertype');   
+}
+
+function showBooks(){
+
+    let data = localStorage.getItem('books');
+
+    if(!data){
+        let arr = [];
+        data = JSON.stringify(arr);
+    }
+
+    books = JSON.parse(data);
+
+    let output = '<h2 class="mb-4">Book List</h2>';
+
+    books.forEach((book) =>{
+        output += `
+        <ul class= 'list-group mb-3'>
+                <li class='list-group-item'>ID: ${book.id}</li>
+                <li class='list-group-item'>Mail: ${book.name}</li>
+                <li class='list-group-item'>Name: ${book.author}</li>    
+        </ul>
+        `;
+    });
+
+    document.getElementById('output').innerHTML = output;
+
+}
+
+showBooks();
+
+
--- check.js
+++ check.js
@@ -14,6 +14,7 @@ if(!getCreds){
     window.location.replace('./wrong_creds.html')
 }
 
+// console.log("ashse")
 let creds = getCreds.split(" ");
 
 console.log(creds);
@@ -22,7 +23,13 @@ if(values[1] === creds[1]){
     localStorage.setItem("loggedIn", "true");
     localStorage.setItem("username", creds[0]);
     localStorage.setItem("usertype", creds[2]);
-    window.location.replace('./index.html');
+    // window.location.replace('./index.html');
+    if(creds[2] === "user"){
+        window.location.replace('./booklistuser.html');
+    }else if(creds[2] == 'admin'){
+        window.location.replace('./admin.html')
+    }
+    
 }else{
     window.location.replace('./wrong_creds.html')
 }
