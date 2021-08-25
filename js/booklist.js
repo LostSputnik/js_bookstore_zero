ec96f97ab17990e4a926b69156e0b9aef692007c
kafka <sajon.tahsen@gmail.com>
2021-08-25 14:36:21 +0600

    implemented API integration

--- js/booklist.js
+++ js/booklist.js
@@ -1,46 +1,54 @@
 import {logOut} from './logout.js';
 import {greetUser} from './greetuser.js';
-greetUser();
 
 let logout = document.querySelector('.logout');
 logout.addEventListener('click', logOut);
 
-let library = document.querySelector('.library');
-
-let data = localStorage.getItem('books');
-let books = JSON.parse(data);
-
-
-if(!books){
-    books = [];
-}
-
-if(!books.length){
-    // console.log('ok');
-    library.innerHTML = "Sorry, No Books Yet!"
-}
-else{
-    let output = '';
-    books.forEach(book => {
-        output += `
-        <div class="book">
-            <img src="${book.thumbnail}" alt="" class="cover">
-            <div class="book-title">${book.title}</div>
-            <a href="./details.html" class="booklink ${book.id}">Know More</a>
-        </div>
-        `;
-    });
-    library.innerHTML = output;
-    let links = library.querySelectorAll('.booklink');
-    console.log(links);
-    links.forEach(link => {
-        // console.log(link.classList[1]);
-        link.addEventListener('click', () => {
-            let id = link.classList[1];
-            localStorage.setItem('currentBook', id);
+const showBooks = (library, books) => {
+    if(!books){
+        books = [];
+    }
+    if(!books.length){
+        // console.log('ok');
+        library.innerHTML = "Sorry, No Books Yet!"
+    }
+    else{
+        let output = '';
+        books.forEach(book => {
+            output += `
+            <div class="book">
+                <img src="${book.thumbnail}" alt="" class="cover">
+                <div class="book-title">${book.title}</div>
+                <a href="./details.html?id=${book.id}" class="booklink">Know More</a>
+            </div>
+            `;
         });
-    });
+        library.innerHTML = output;
+
+        // THIS CODE ADDED ID TO BOOK'S CLASS IN OLDER IMPLEMENTATION
+        
+        // let links = library.querySelectorAll('.booklink');
+        // // console.log(links);
+        // links.forEach(link => {
+        //     // console.log(link.classList[1]);
+        //     link.addEventListener('click', () => {
+        //         let id = link.classList[1];
+        //         localStorage.setItem('currentBook', id);
+        //     });
+        // });
+    }
 }
 
+const fetchBooksandShow = async (library, url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const books = data.data.data;
+    showBooks(library, books);
+}
 
+greetUser();
+// let data = localStorage.getItem('books');
+let library = document.querySelector('.library');
+let url = 'http://127.0.0.1:3000/api/v1/books';
 
+fetchBooksandShow(library, url);
--- js/details.js
+++ js/details.js
@@ -1,37 +1,44 @@
 import {greetUser} from './greetuser.js';
+
+const getBook = async () => {
+    const params = new URLSearchParams(document.location.search.substring(1));
+    const id = params.get('id');
+    const response = await fetch('http://127.0.0.1:3000/api/v1/books/' + id);
+    const data = await response.json();
+    const book = data.data.data;
+    showBook(book);
+}
+
+const showBook = (book) => {
+
+    const info = document.querySelector('.book-info');
+    // console.log(book.infolink)
+
+    const output = `
+                <img src="${book.thumbnail}" alt="" class="thumbnail">
+                <div class="book-title">${book.title}</div>
+                <hr>
+                <div class="authors">Author: ${book.authors}</div>
+                <hr>
+                <div class="year">Published: ${book.publishedDate}</div>
+                <hr>
+                <div class="categories">Categories: ${book.categories}</div>
+                <hr>
+                <div class="description">${book.description}</div>
+                <hr>
+                <div class="publisher">Publisher: ${book.publisher}</div>
+                <hr>
+                <div class="pages">Pages: ${book.pageCount}</div>
+                <hr>
+                <div class="googlebookslink"><a href="${book.infolink}">Learn more on Google Books</a>
+                </div>
+                <hr>
+
+    `
+
+    info.innerHTML = output;
+
+}
+
 greetUser();
-
-const id = localStorage.getItem('currentBook');
-const data = localStorage.getItem('books');
-const books = JSON.parse(data);
-// console.log(books);
-
-const book = books[id];
-// console.log(book);
-
-const info = document.querySelector('.book-info');
-console.log(book.infolink)
-
-let output = `
-            <img src="${book.thumbnail}" alt="" class="thumbnail">
-            <div class="book-title">${book.title}</div>
-            <hr>
-            <div class="authors">Author: ${book.authors}</div>
-            <hr>
-            <div class="year">Published: ${book.publishedDate}</div>
-            <hr>
-            <div class="categories">Categories: ${book.categories}</div>
-            <hr>
-            <div class="description">${book.description}</div>
-            <hr>
-            <div class="publisher">Publisher: ${book.publisher}</div>
-            <hr>
-            <div class="pages">Pages: ${book.pageCount}</div>
-            <hr>
-            <div class="googlebookslink"><a href="${book.infolink}">Learn more on Google Books</a>
-            </div>
-            <hr>
-
-`
-
-info.innerHTML = output;
+getBook()
