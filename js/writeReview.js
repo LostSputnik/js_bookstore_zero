8d2202e0e87c8100baa4f9c8ead3976f87709cda
kafka <sajon.tahsen@gmail.com>
2022-09-06 20:29:19 +0600

    implemented write review functionality

--- js/writeReview.js
+++ js/writeReview.js
@@ -14,7 +14,41 @@ const books = JSON.parse(data);
 const book = books.find((book) => book.isbn == isbn);
 // console.log(book);
 
+let reviewsJSON = localStorage.getItem("reviews");
+let reviews = JSON.parse(reviewsJSON);
+
+if (!reviews) {
+  reviews = [];
+}
+
 let user = localStorage.getItem("currentusername");
+let userid = localStorage.getItem("currentuserid");
+
+let form = document.querySelector(".review-form");
+form.addEventListener("submit", processReview);
+
+function processReview(e) {
+  e.preventDefault();
+  const reviewTitle = document.querySelector("#reviewTitle").value;
+  const reviewBody = document.querySelector(".reviewBody").value;
+  let newReview = { user, userid, reviewTitle, reviewBody, isbn: book.isbn };
+
+  reviews.push(newReview);
+  localStorage.setItem("reviews", JSON.stringify(reviews));
+
+  let alert = document.querySelector(".alert");
+  let output = `
+        <div class="chip">
+            <i class="fas fa-check"></i> Thank you for writing in! Redirecting... 
+        </div>
+    `;
+  alert.innerHTML = output;
+
+  setTimeout(() => {
+    window.location.replace("../index.html");
+  }, 1500);
+}
+
 let reviewDiv = "";
 if (!user) {
   reviewDiv = `<div class='write-review'><a href="./login.html">Login to write a review</a></div>
